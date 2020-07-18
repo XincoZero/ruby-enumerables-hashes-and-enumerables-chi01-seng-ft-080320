a3bbdd8fad92070f4828904020002db8ce87f866
Hector Alvarez <h-alvarez@wiu.edu>
2020-07-18 15:20:15 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,5 +9,10 @@
 def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
-    if suite == :suite_a 
+    if suite == :suite_a &&
+      name.start_with? ("A")
+      winner = name
+    end 
 end
+
+winner 
